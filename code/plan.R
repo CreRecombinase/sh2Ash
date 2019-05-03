98eb8930aa426b8ad9836056566b39a27bf68cee
CreRecombinase <nwknoblauch@gmail.com>
2019-05-03 17:26:21 -0500

    got rid of shortcut

--- code/plan.R
+++ code/plan.R
@@ -1,9 +1,9 @@
 
-c_dir <- fs::path_expand("/project/compbio/cause_results/cause_grid3/")
+c_dir <- fs::path_expand("~/Desktop/cause_dir/")
 
-eqtl_dir <- fs::path_expand("/scratch/midway2/nwknoblauch/eqtl/")
+eqtl_dir <- fs::path_expand("/run/media/nwknoblauch/Data/eQTL_SumStats/gtex_v7_feathers/")
 all_files <- fs::path_file(fs::dir_ls(c_dir, glob = "*cause.RDS"))
-eqtl_files <- fs::path_file(fs::dir_ls(eqtl_dir, glob = "*fst"))
+eqtl_files <- fs::path_file(fs::dir_ls(eqtl_dir, glob = "*feather"))
 plan <- drake_plan(
     causedata = target(readRDS(fs::path(c_dir, all_files[x])),
                        transform = map(x = !!(seq_along(all_files)))),
@@ -21,10 +21,10 @@ plan <- drake_plan(
                      transform = combine(s_eqtl_df)),
     genelist = unique_col(eqtl_df, anno_df, col = "ensembl_gene_stable_id"),
     godf = target(retrieve_go_ids(genelist), hpc = FALSE),
-    snp_gene_df = finalize_snp_gene(anno_df, eqtl_df, godf),
-    sgm = snp_gene_mat(snp_gene_df, all_snps),
-    res_df = target(
-        enrich_fun(cause_df, medz, sgm, file = all_files[x]),
-        transform = map(cause_df, medz, x = !!(seq_along(all_files)))
-    ),
+    # snp_gene_df = finalize_snp_gene(anno_df, eqtl_df, godf),
+    # sgm = snp_gene_mat(snp_gene_df, all_snps),
+    # res_df = target(
+    #     enrich_fun(cause_df, medz, sgm, file = all_files[x]),
+    #     transform = map(cause_df, medz, x = !!(seq_along(all_files)))
+    # ),
     )
